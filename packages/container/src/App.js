8eab19c651d7b35df803b409dfb7c1446c126a64
Drew Chorney <drchorney@protonmail.com>
2021-10-07 17:54:17 -0600

    updating css prefixes for production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+}
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+// this affects css prefix after building in production
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
-  return <div>
-    <h1>Hi There!</h1>
-    <hr />
-    <MarketingApp />
-  </div>;
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
 }
