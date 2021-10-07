d75b5a0fddb2ec63cb644047215fae8ecc50439e
Drew Chorney <drchorney@protonmail.com>
2021-10-07 17:55:49 -0600

    fix bug

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
-}
+} from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
